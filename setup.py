cc235a0c01f2d5f2a3e144d1e504116b8f24eb76
Bissbert <43237892+Bissbert@users.noreply.github.com>
2024-07-02 15:47:47 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RecipeGenerator',
+    name='FilterlessCook',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -9,15 +9,15 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'recipe-generator=recipeGenerator:main',
+            'filterless-cook=filterlesscook:main',
         ],
     },
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Bissbert',
+    author_email='dev@bissbert.ch',
     description='Generate LaTeX formatted food recipes using the ollama library.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='http://github.com/yourusername/recipegenerator',
+    url='http://github.com/Bissbert/filterlesscook',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -26,5 +26,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
     ],
-    keywords='recipe generator latex',
+    keywords='recipe generator latex unfiltered filterless',
 )
